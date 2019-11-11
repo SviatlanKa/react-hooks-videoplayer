76a3f9e99bf9310e95e3624ad909caacd5f68b98
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-11-11 14:43:03 +0300

    Updates

---
+++ precache-manifest.64dac157a0bfbee831115b4d5f5b3f99.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c46ed035b60b7b141b80e239c166c47e",
+    "url": "/react-hooks-videoplayer/index.html"
+  },
+  {
+    "revision": "2f7fba225cf7fced7b2d",
+    "url": "/react-hooks-videoplayer/static/js/2.36bc12e1.chunk.js"
+  },
+  {
+    "revision": "d8a661ce7b1735017a9e",
+    "url": "/react-hooks-videoplayer/static/js/main.07b3d454.chunk.js"
+  },
+  {
+    "revision": "f2ea63b0bca252f82347",
+    "url": "/react-hooks-videoplayer/static/js/runtime-main.53380e65.js"
+  }
+]);
